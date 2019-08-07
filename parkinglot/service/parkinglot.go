5214386fae0addcec579979fc6ca5cfec871924d
berdoezt <albertmario19@gmail.com>
2019-08-07 22:45:59 +0700

    add unit test and implement service

--- parkinglot/service/parkinglot.go
+++ parkinglot/service/parkinglot.go
@@ -6,16 +6,35 @@ import (
 
 // Park parking a car to available slot
 func (s *Service) Park(car parkinglot.Car) error {
+	slotID, err := s.store.GetAvailableSlot()
+	if err != nil {
+		return err
+	}
+
+	if err := s.store.FillSlot(slotID, car); err != nil {
+		return err
+	}
+
 	return nil
 }
 
 // Leave car leaving slot
 func (s *Service) Leave(slotID int64) error {
+	err := s.store.FreeSlot(slotID)
+	if err != nil {
+		return err
+	}
+
 	return nil
 }
 
 // CreateParkingLot crete parking lot with specific slot
 func (s *Service) CreateParkingLot(slot int64) error {
+	err := s.store.CreateSlots(slot)
+	if err != nil {
+		return err
+	}
+
 	return nil
 }
 
--- parkinglot/service/parkinglot_test.go
+++ parkinglot/service/parkinglot_test.go
@@ -1,33 +1,58 @@
 package service
 
 import (
+	"errors"
 	"reflect"
 	"testing"
 
+	"github.com/golang/mock/gomock"
 	"github.com/parking_lot/parkinglot"
+	mock_store "github.com/parking_lot/parkinglot/mockstore"
 )
 
+var err = errors.New("")
+
 func TestService_CreateParkingLot(t *testing.T) {
-	type fields struct {
-		store parkinglot.Store
-	}
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	mockStore := mock_store.NewMockStore(ctrl)
+	service := New(mockStore)
+
 	type args struct {
 		slot int64
 	}
 	tests := []struct {
-		name    string
-		fields  fields
-		args    args
-		wantErr bool
+		name     string
+		args     args
+		mockFunc func()
+		wantErr  bool
 	}{
-		// TODO: Add test cases.
+		{
+			name: "#1 normal case",
+			args: args{
+				slot: 5,
+			},
+			mockFunc: func() {
+				mockStore.EXPECT().CreateSlots(gomock.Any()).Return(nil)
+			},
+			wantErr: false,
+		},
+		{
+			name: "#2 error case",
+			args: args{
+				slot: 5,
+			},
+			mockFunc: func() {
+				mockStore.EXPECT().CreateSlots(gomock.Any()).Return(err)
+			},
+			wantErr: true,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			s := &Service{
-				store: tt.fields.store,
-			}
-			if err := s.CreateParkingLot(tt.args.slot); (err != nil) != tt.wantErr {
+			tt.mockFunc()
+			if err := service.CreateParkingLot(tt.args.slot); (err != nil) != tt.wantErr {
 				t.Errorf("Service.CreateParkingLot() error = %v, wantErr %v", err, tt.wantErr)
 			}
 		})
@@ -35,26 +60,68 @@ func TestService_CreateParkingLot(t *testing.T) {
 }
 
 func TestService_Park(t *testing.T) {
-	type fields struct {
-		store parkinglot.Store
-	}
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	mockStore := mock_store.NewMockStore(ctrl)
+	service := New(mockStore)
+
 	type args struct {
 		car parkinglot.Car
 	}
+
 	tests := []struct {
-		name    string
-		fields  fields
-		args    args
-		wantErr bool
+		name     string
+		mockFunc func()
+		args     args
+		wantErr  bool
 	}{
-		// TODO: Add test cases.
+		{
+			name: "#1 normal case",
+			mockFunc: func() {
+				mockStore.EXPECT().GetAvailableSlot().Return(int64(5), nil)
+				mockStore.EXPECT().FillSlot(int64(5), gomock.Any()).Return(nil)
+			},
+			args: args{
+				car: parkinglot.Car{
+					Color:              "white",
+					RegistrationNumber: "KA-01-HH-7777",
+				},
+			},
+			wantErr: false,
+		},
+		{
+			name: "#2 slot is empty",
+			mockFunc: func() {
+				mockStore.EXPECT().GetAvailableSlot().Return(int64(0), err)
+			},
+			args: args{
+				car: parkinglot.Car{
+					Color:              "white",
+					RegistrationNumber: "KA-01-HH-7777",
+				},
+			},
+			wantErr: true,
+		},
+		{
+			name: "#3 can't fill parking slot",
+			mockFunc: func() {
+				mockStore.EXPECT().GetAvailableSlot().Return(int64(5), nil)
+				mockStore.EXPECT().FillSlot(int64(5), gomock.Any()).Return(err)
+			},
+			args: args{
+				car: parkinglot.Car{
+					Color:              "white",
+					RegistrationNumber: "KA-01-HH-7777",
+				},
+			},
+			wantErr: true,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			s := &Service{
-				store: tt.fields.store,
-			}
-			if err := s.Park(tt.args.car); (err != nil) != tt.wantErr {
+			tt.mockFunc()
+			if err := service.Park(tt.args.car); (err != nil) != tt.wantErr {
 				t.Errorf("Service.Park() error = %v, wantErr %v", err, tt.wantErr)
 			}
 		})
@@ -62,26 +129,46 @@ func TestService_Park(t *testing.T) {
 }
 
 func TestService_Leave(t *testing.T) {
-	type fields struct {
-		store parkinglot.Store
-	}
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	mockStore := mock_store.NewMockStore(ctrl)
+	service := New(mockStore)
+
 	type args struct {
 		slotID int64
 	}
 	tests := []struct {
-		name    string
-		fields  fields
-		args    args
-		wantErr bool
+		name     string
+		mockFunc func()
+		args     args
+		wantErr  bool
 	}{
-		// TODO: Add test cases.
+		{
+			name: "#1 normal case",
+			mockFunc: func() {
+				mockStore.EXPECT().FreeSlot(gomock.Any()).Return(nil)
+			},
+			args: args{
+				slotID: 5,
+			},
+			wantErr: false,
+		},
+		{
+			name: "#1 error leave parking slot",
+			mockFunc: func() {
+				mockStore.EXPECT().FreeSlot(gomock.Any()).Return(err)
+			},
+			args: args{
+				slotID: 5,
+			},
+			wantErr: true,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			s := &Service{
-				store: tt.fields.store,
-			}
-			if err := s.Leave(tt.args.slotID); (err != nil) != tt.wantErr {
+			tt.mockFunc()
+			if err := service.Leave(tt.args.slotID); (err != nil) != tt.wantErr {
 				t.Errorf("Service.Leave() error = %v, wantErr %v", err, tt.wantErr)
 			}
 		})
@@ -182,6 +269,35 @@ func TestService_GetSlotNumberByRegistrationNumber(t *testing.T) {
 			}
 			if got != tt.want {
 				t.Errorf("Service.GetSlotNumberByRegistrationNumber() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestService_GetStatus(t *testing.T) {
+	type fields struct {
+		store parkinglot.Store
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		want    []parkinglot.Parking
+		wantErr bool
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			s := &Service{
+				store: tt.fields.store,
+			}
+			got, err := s.GetStatus()
+			if (err != nil) != tt.wantErr {
+				t.Errorf("Service.GetStatus() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("Service.GetStatus() = %v, want %v", got, tt.want)
 			}
 		})
 	}
